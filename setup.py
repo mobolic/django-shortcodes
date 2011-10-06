fad7c2c038723b688a5d8ad6fbb53e4327d885d3
Martey Dodoo <martey@marteydodoo.com>
2011-10-06 09:40:24 -0400

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="django-shortcodes",
+    version="0.1",
+    description="WordPress shortcode support for Django",
+    author="bluemilkshake",
+    author_email="bluemilkshake@googlemail.com",
+    packages=["shortcodes"],
+)
