f6c47f0deee93bad1cec94a95f8e04bb1fcd76a4
Martey Dodoo <martey@marteydodoo.com>
2012-03-06 17:44:47 -0500

    Better setup.py, actual MANIFEST.in.

--- setup.py
+++ setup.py
@@ -1,10 +1,30 @@
 from distutils.core import setup
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name="django-shortcodes",
-    version="0.1",
+    version="0.2.0",
     description="WordPress shortcode support for Django",
-    author="bluemilkshake",
-    author_email="bluemilkshake@googlemail.com",
-    packages=["shortcodes"],
+    author="Mark Steadman",
+    author_email="marksteadman@me.com",
+    maintainer="Martey Dodoo",
+    maintainer_email="martey@marteydodoo.com",
+    url="https://github.com/martey/django-shortcodes",
+    license="MIT",
+    packages=[
+        "shortcodes",
+        "shortcodes.parsers",
+        "shortcodes.templatetags",
+    ],
+    long_description=read("README.markdown"),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+    ],
 )
